41d258eaabc1b4d551bfaf4b659ba9cbdef767d6
feimao <1532921016@qq.com>
2021-02-05 01:54:10 +0800

    Spring相关

---
+++ Spring_aop/src/main/java/com/kawainekosann/Proxy/cglib/ProxyTest.java
@@ -0,0 +1,32 @@
+package com.kawainekosann.Proxy.cglib;
+
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class ProxyTest {
+    static private Target target = new Target();
+    //获得增强对象
+    static private Advice advice = new Advice();
+    //返回值 就是动态生成的代理对象 基于cglib
+    public static void main(final String[] args) {
+        //1.创建增强器
+        Enhancer enhancer = new Enhancer();
+        //2.设置父类
+        enhancer.setSuperclass(Target.class);
+        //3.设置回调
+        enhancer.setCallback(new MethodInterceptor() {
+            public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                advice.before();
+                Object invoke = method.invoke(target,args);
+                advice.after();
+                return invoke;
+            }
+        });
+        //4.创建代理对象 此处是父子关系
+        Target proxy = (Target) enhancer.create();
+        proxy.save();
+    }
+}
