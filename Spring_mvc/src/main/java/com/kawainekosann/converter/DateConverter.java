7472e3d7940682c28dbb2ed2de417d5b6aac177e
LIU <1532921016@qq.com>
2021-02-18 17:45:35 +0800

    类型转换器

---
+++ Spring_mvc/src/main/java/com/kawainekosann/converter/DateConverter.java
@@ -0,0 +1,21 @@
+package com.kawainekosann.converter;
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateConverter implements Converter<String, Date> {
+    //将日期字符串转换成日期对象，返回
+    public Date convert(String dateStr) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = null;
+        try {
+            date = format.parse(dateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+}
